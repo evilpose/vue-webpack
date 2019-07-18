a981f4e6cd231553357348fb9e17911bfeb012f8
BU <1165106117@qq.com>
2019-07-18 10:28:51 +0800

    项目轮廓

---
+++ src/components/tabbar/HomeContainer.vue
@@ -0,0 +1,125 @@
+<template>
+    <div>
+
+    <!-- 中间轮播图区域 -->
+
+    <mt-swipe :auto="4000">
+    <!-- 在组件中，使用v-for循环的话，一定要使用 key -->
+    <!-- <mt-swipe-item v-for="item in LunbotuList" :key="item.url">
+        <img :src="item.img" alt="">
+    </mt-swipe-item> -->
+    <mt-swipe :auto="4000">
+    <mt-swipe-item>1</mt-swipe-item>
+    <mt-swipe-item>2</mt-swipe-item>
+    <mt-swipe-item>3</mt-swipe-item>
+    </mt-swipe>
+    </mt-swipe>
+
+
+    <!-- 九宫格到六宫格的改造 -->
+    <ul class="mui-table-view mui-grid-view mui-grid-9">
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <!-- <span class="mui-icon mui-icon-home"></span> -->
+                <img src="../../images/menu1.png" alt="">
+                <div class="mui-media-body">新闻资讯</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <!-- <span class="mui-icon mui-icon-email"><span class="mui-badge">5</span></span> -->
+                <img src="../../images/menu2.png" alt="">
+                <div class="mui-media-body">图片分享</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <!-- <span class="mui-icon mui-icon-chatbubble"></span> -->
+                <img src="../../images/menu3.png" alt="">
+                <div class="mui-media-body">商品购买</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <!-- <span class="mui-icon mui-icon-location"></span> -->
+                <img src="../../images/menu4.png" alt="">
+                <div class="mui-media-body">留言专区</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <!-- <span class="mui-icon mui-icon-search"></span> -->
+                <img src="../../images/menu5.png" alt="">
+                <div class="mui-media-body">视频专区</div></a></li>
+        <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+                <!-- <span class="mui-icon mui-icon-phone"></span> -->
+                <img src="../../images/menu6.png" alt="">
+                <div class="mui-media-body">联系我们</div></a></li>
+    </ul> 
+
+    <!-- <h3>HomeContainer</h3> -->
+
+    </div>
+</template>
+<script>
+import {Toast} from 'mint-ui'
+export default {
+    data(){
+        return {
+            // LunbotuList:[]  // 保存轮播图的数组
+        }
+    },
+    // created(){
+    //     this.getLunbotu()
+    // },
+    methods:{
+        // getLunbotu(){   //获取轮播图的数据的方法
+        // this.$http.get('http://39.105.29.12:8585/app/v1/albums/active').then(resule =>{
+        //     // console.log(resule.body)
+        //     if(resule.body.statu === 0){
+        //         //成功的
+        //         this.LunbotuList = resule.body.message;
+        //     }else{
+        //         //失败的
+        //         Toast('获取加载轮播图失败')
+        //     }
+        // })
+        // }
+    },
+
+}
+</script>
+<style lang="scss" scoped>
+.mint-swipe{
+    height: 200px;
+    //nth-child(2) 父元素的第二个子元素
+    // .mint-swipe-item:nth-child(1){
+    //     background-color: red;
+    // }
+    // .mint-swipe-item:nth-child(2){
+    //     background-color: rebeccapurple;
+    // }
+    // .mint-swipe-item:nth-child(3){
+    //     background-color: yellowgreen;
+    // }
+    .mint-swipe-item{   // & 符号形成嵌套，形成交集选择器
+        &:nth-child(1){
+            background-color: red;
+        }
+        &:nth-child(2){
+            background-color: rebeccapurple;
+        }
+        &:nth-child(3){
+            background-color: yellowgreen;
+        }
+        img{
+        width:100%;
+        height: 100%;
+            }
+        }   
+    }
+    
+    .mui-grid-view.mui-grid-9{
+        background-color: white;
+        border: none;
+        img{
+            width: 60px;
+            height: 60px;
+        }
+           
+    }
+    .mui-grid-view.mui-grid-9 .mui-table-view-cell{
+        border: none;
+    }
+    .mui-table-view.mui-grid-view .mui-table-view-cell .mui-media-body{
+        font-size: 13px
+    }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,19 +1,38 @@
 //入口文件
 import Vue from 'vue'
 
+//1.1  导入路由的包
+import VueRouter from 'vue-router'
+//1.2  启用vuerouter
+Vue.use(VueRouter)
+
 //导入app根组件
 import app from './App.vue'
 
 //按需导入mint-ui中的组件
-import { Header } from 'mint-ui'
+import { Header,Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+//1.3 导入自己的router.js路由模块
+import router from './router.js'
 
 //导入 MUI 的样式
 import './lib/MUI/css/mui.min.css'
+//导入　MUI　的扩展图标
+import './lib/MUI/css/icons-extra.css'
+
+//2.1 导入 vue-resource
+import VueResource from 'vue-resource'
+//2.2 安装 vue-resource 
+Vue.use(VueResource)
+
 
 var vm = new Vue({
     el:'#app',
     data:{},
     methods: {},
-    render:c=>c(app)
+    render:c=>c(app),
+    router  //1.4 挂载路由对象到vm实例上
 })
